fd0df5f91b5ff40119ee8111edbff771b03b534c
kafun <513112374@qq.com>
2019-02-28 01:00:01 +0800

    动态拉取数据

--- untitled/homework1.py
+++ untitled/homework1.py
@@ -7,20 +7,47 @@ url = 'https://gz.58.com/pingbandiannao/36199097499657x.shtml'
 wb_data = requests.get(url)
 soup = BeautifulSoup(wb_data.text,'lxml')
 
-def get_item_info(url):
-    wb_data = requests.get(url)
+def get_links_from(who_sells=0):
+    urls = []
+    list_view = 'https://gz.58.com/pbdn/{}/'.format(str(who_sells))
+    wb_data = requests.get(list_view)
     soup = BeautifulSoup(wb_data.text,'lxml')
+    for link in soup.select('td.t a.t'):
+        urls.append(link.get('href').split('?')[0])
+        return urls
+        #print(urls)
+
+def get_views_from(url):
+    id = url.split('/')[-1].strip('x.shtml')
+    api = 'https://jst1.58.com/counter?infoid={}'.format(id)
+    js = requests.get(api)
+    views = js.text.split('=')[-1]
+    return url
+    #print(js.text)
+
+def get_item_info(who_sells=0):
+    urls = get_links_from(who_sells)
+    for url in urls:
+        wb_data = requests.get(url)
+        soup = BeautifulSoup(wb_data.text,'lxml')
 # title = soup.title.text
 # price = soup.select('div.infocard__container__item__main > span')
 # date = soup.select('div.detail-title__info > div:nth-of-type(1)')
 # area = soup.select('div.infocard__container__item__main > a')
     # basicinfo > div.infocard__container.haveswitch > div:nth-child(1) > div.infocard__container__item__main > span
-    data = {
-        'title':soup.title.text,
-        'price':soup.select('#basicinfo span.infocard__container__item__main__text--price')[0].text,
-        'date':soup.select('div.detail-title__info > div:nth-of-type(1)').text,
-        'area':list(soup.select('div.infocard__container__item__main > a')[0].stripped_strings)
-        }
-    print(data)
+        data = {
+            'title':soup.title.text,
+            'price':soup.select('.infocard__container__item__main__text--price')[0].text.strip() if soup.find_all('span','infocard__container__item__main__text--price') else None,
+            'date':soup.select('div.detail-title__info__text')[0].text if soup.find_all('div','detail-title__info__text') else None,
+            'area':list(soup.select('div.infocard__container__item__main > a')[0].stripped_strings) if soup.find_all('div','infocard__container__item__main') else None,
+            'cate':'个人' if who_sells == 0 else '商家',
+            'views':get_views_from(url)
+            }
+        print(data)
 
-get_item_info(url)
+#get_item_info(url)
+
+#get_links_from(0)
+#get_views_from(url)
+
+get_item_info()
--- untitled/zzz.py
+++ untitled/zzz.py
@@ -6,11 +6,12 @@ url = 'https://gz.58.com/pingbandiannao/36199097499657x.shtml'
 
 wb_data = requests.get(url)
 soup = BeautifulSoup(wb_data.text,'lxml')
+#div.detail-title__info >
 
 title = soup.title.text
-price = soup.select('div.infocard__container__item__main > span').get_text()
-date = soup.select('div.detail-title__info > div:nth-of-type(1)')
-area = soup.select('div.infocard__container__item__main > a')
+price = soup.select('.infocard__container__item__main__text--price')[0].text.strip()
+date = soup.select('div.detail-title__info__text')[0].text
+area = soup.select('div.infocard__container__item__main > a')[0].text
     # basicinfo > div.infocard__container.haveswitch > div:nth-child(1) > div.infocard__container__item__main > span
 # data = {
 #         'title':soup.title.text,
@@ -18,4 +19,4 @@ area = soup.select('div.infocard__container__item__main > a')
 #         'date':soup.select('div.detail-title__info > div:nth-of-type(1)').text,
 #         'area':list(soup.select('div.infocard__container__item__main > a')[0].stripped_strings)
 #        }
-print(price)
+print(area)
